741c0178697e0c4a3d75acab11875b08f1581470
Moffi-bit <lorenzostiavelli6@gmail.com>
2022-10-13 20:38:49 -0400

    got image data

--- fits/ProcessingFits.java
+++ fits/ProcessingFits.java
@@ -1,7 +1,11 @@
 package fits;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 import nom.tam.fits.Fits;
 import nom.tam.fits.FitsException;
+import nom.tam.fits.ImageHDU;
 
 public class ProcessingFits {
 
@@ -9,7 +13,13 @@ public class ProcessingFits {
 		// TODO Auto-generated method stub
 		try {
 			Fits f = new Fits("FOCFITS.fits");
+			ImageHDU hdu = (ImageHDU) f.getHDU(0);
+			float[][] image = (float[][]) hdu.getKernel();
+			System.out.println(image[1023][0]);
 		} catch (FitsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
